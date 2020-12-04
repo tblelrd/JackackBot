e5bedf4c7bae2df7c799366b38f6bc55cf2c1fef
tblelrd <69319061+tblelrd@users.noreply.github.com>
2020-12-04 07:55:05 +0000

    Adding Embeds to the texting!

--- index.js
+++ index.js
@@ -5,20 +5,25 @@ const Avatar = "https://cdn.discordapp.com/avatars/784050441247916032/526f8f0d44
 const PREFIX = "j!"
 
 
+//THINGS THAT HAPPEN WHEN BOT ONLINE
 Bot.once('ready', () => {
     console.log('Bot Online');
+    bot.user.setActivity('j!help',{
+        type: "WATCHING"
+    }).catch(console.error);
 });
 
 Bot.login(process.env.token);
 
 
+//JOIN MESSAGE WHEN INVITED
 Bot.on("guildCreate", member =>{
-    const channel = member.guild.channels.cache.find(channel => channel.type === 'text' && channel.permissionsFor(guild.me).has('SEND_MESSAGES'))
+    const channel = member.channels.cache.find(channel => channel.type === 'text' && channel.permissionsFor(member.me).has('SEND_MESSAGES'))
     const ThankYouEmbed = new Discord.MessageEmbed()
         .setColor('#fc00ff')
         .setTitle('Thanks for inviting me to ur server!')
-        .setThumbnail(member.guild.iconURL())
-        .setImage(member.guild.me.user.avatarURL())
+        .setThumbnail(member.iconURL())
+        .setImage(member.me.user.avatarURL())
         
     channel.send(ThankYouEmbed)
     
@@ -32,8 +37,9 @@ Bot.on("message", async msg =>{
     }
 
 
-    //TEXT COMMAND
+    //DM CHECK
     if(msg.channel.type == "dm") {
+        //TEXT COMMAND
         if(msg.content == "j!text") {
             var messageContent
             var messageReciever
@@ -71,10 +77,12 @@ Bot.on("message", async msg =>{
 
                     Bot.users.fetch(messageReciever).then((user) => {
                         
-                        const embed= new Discord.MessageEmbed(title=`${msg.author.username}#${msg.author.discriminator}(${msg.author.id}): ${messageContent}`, description="Sent via the jackack message service", color=0xff00ae)
+                        const text= new Discord.MessageEmbed()
+                            .setTitle(`${msg.author.username}#${msg.author.discriminator}(${msg.author.id}): ${messageContent}`)
+                            .setDescription("Sent via the jackack message service") 
+                            .setColor("#FF00C8")
                         
-                        
-                        user.send(embed);
+                        user.send(text);
                         msg.channel.send(`${user.username}#${user.discriminator} has been sent the following message: ${messageContent}`)
                     });
                     
@@ -104,18 +112,62 @@ Bot.on("message", async msg =>{
 
     // COMMANDS
 
-    if(!msg.content.substring(PREFIX.length) == PREFIX){
-       return
-    } 
+    if(!msg.content.substring(PREFIX.length) == PREFIX) return
     let args = msg.content.toLowerCase().substring(PREFIX.length).split(" ");
 
     switch(args[0]) {
-        case "ping":
-            msg.reply("Pong!")
+        case 'ping':
+            const ping = await msg.channel.send('pinging');
+            ping.edit("🏓Pong!")
+
+            msg.channel.send(`Pong!\nLatency is ${Math.floor(ping.createdTimestamp - msg.createdTimestamp)}ms`)
         break;
         
         case "startdm":
             msg.author.send("Hi!")
-    
+        break
+        
+        case "help":
+            const help = new Discord.MessageEmbed()
+                .setTitle("Help")
+                .setDescription("This is the Help Screen for Jackack bot, version is 1.0")
+                .setAuthor("Jackack Bot", msg.guild.me.user.avatarURL())
+                .setColor("#FF00C8")
+                .addField("Help", "Displays this help screen", true)
+                .addField("Ping", "Pong!", true)
+                .addField("StartDM", "Essential for texting", true)
+                .addField("Say", "Says your message as you")
+                .addField("Text", "Only works in DMs also type the command as standalone so it works")
+                .setThumbnail(msg.guild.iconURL())
+                .setTimestamp()
+                .setFooter("Bot created at Dec 2nd, 2020", msg.guild.me.user.avatarURL())
+
+            msg.channel.send(help)
+        break
+        
+        case "me":
+            const meee = await msg.reply('If it didnt work its becuz ur not in a server')
+            const me = new Discord.MessageEmbed()
+            .setTitle('User Information')
+            .addField('Name:', msg.author.username, true)
+            .addField('Server:', msg.guild.name,true)
+            .addField('Member Count', msg.guild.memberCount,true)
+            .addField('Role In The Sever', msg.member.roles.highest.name,true)
+            .addField('Joined At:', msg.member.joinedAt, true)
+            .addField('Owner:',msg.guild.owner.nickname + '(' +msg.guild.owner.user.tag + ')', true)
+            .setColor(0xD54C2E)
+            .setThumbnail(msg.author.displayAvatarURL());
+            msg.channel.send(me)
+            meee.delete()
+        break
+
+        case "id":
+            if(msg.mentions.users.length != 0){
+                msg.mentions.users.forEach((k,v) => {
+                    console.log(k,v,'')
+                    msg.reply(k.id)
+                });
+            }
+        break
     }
 });
