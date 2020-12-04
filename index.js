fb81bda9d4c49ae32ca0657e865081e6210b2712
tblelrd <69319061+tblelrd@users.noreply.github.com>
2020-12-04 08:20:48 +0000

    Fixing ME command

--- index.js
+++ index.js
@@ -155,7 +155,7 @@ Bot.on("message", async msg =>{
             .addField('Name:', (await msg.guild.members.fetch(msg.author.id)).nickname, true)
             .addField('Server:', msg.guild.name,true)
             .addField('Member Count', msg.guild.memberCount,true)
-            .addField('Role In The Sever', msg.member.roles.highest.name,true)
+            .addField('Role In The Sever', msg.member.roles.highest.toString(),true)
             .addField('Joined At:', msg.member.joinedAt, true)
             .addField('Owner:',msg.guild.owner.nickname + '(' +msg.guild.owner.user.tag + ')', true)
             .setColor("#FF00C8")
