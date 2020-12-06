53c7ba6bf7cf63a0f10b07e320abe91a7d7c2216
tblelrd <69319061+tblelrd@users.noreply.github.com>
2020-12-06 17:13:27 +0000

    Bug fixes for namehistory

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ async function nh(name, msg) {
     let i;
 
     for(i = 0; i < nameHistory.length; i++) {
-        msg.channel.send(nameHistory[i]);
+        msg.channel.send(nameHistory[i].name);
     }
 }
 
