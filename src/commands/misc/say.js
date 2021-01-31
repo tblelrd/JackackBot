eaa9ac95ead22fee5f89926151056b240ab439f8
Jack <getjacked540c@gmail.com>
2021-01-31 02:06:17 +0000

    Cooler say command

--- src/commands/misc/say.js
+++ src/commands/misc/say.js
@@ -1,8 +1,18 @@
+const Discord = require('discord.js');
+
 module.exports = {
     commands: ['say', 'repeat'],
     expectedArgs: '<tex>',
     minArgs: 1,
     callback: async (msg, args, text) => {
-        msg.channel.send(text);
+        const name = msg.member.nickname;
+        const avatar = msg.author.avatarURL({ dynamic: true });
+
+        await msg.delete();
+
+        const webhook = await msg.channel.createWebhook('SayHook', { reason: 'Say' });
+        await webhook.send(text, { username: name, avatarURL: avatar });
+
+        await webhook.delete();
     },
 };
